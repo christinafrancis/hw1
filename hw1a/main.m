02d277accae0d2c2ce13a55d5f27f5fc2db3ad61
Christina Francis <christina@unknown881fa11d5346.gateway.2wire.net>
2013-09-03 12:03:25 -0700

    Initial Commit

---
+++ hw1a/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  hw1a
+//
+//  Created by Christina Francis on 9/3/13.
+//  Copyright (c) 2013 Christina Francis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
