ff68cc66c487af04c6bebe5b32590843fa30cc40
Christina Francis <christina@unknown881fa11d5346.gateway.2wire.net>
2013-09-04 11:02:28 -0700

    Date and time, char* to NSString conversion.

--- hw1a/main.m
+++ hw1a/main.m
@@ -16,6 +16,19 @@ int main(int argc, const char * argv[])
         // insert code here...
         NSLog(@"Hello, World!");
         
+        char name[40] = "statement";
+        
+        scanf("%s", name);
+        
+        NSString *conv_string;
+        
+        conv_string = [ NSString stringWithUTF8String:name ];
+        
+        NSLog(@" %@ is the converted string ", conv_string);
+        
+        NSLog(@" %@ represents current date and time ", [NSDate date]);
+        
+        
     }
     return 0;
 }
